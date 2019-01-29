4f29903436a5048d7ce065317f20adc5018c932d
Cintia Zago <cintiazago@gmail.com>
2019-01-28 23:34:58 -0200

    Configurando acesso a banco remoto

--- ControleContasAPI/Util/DAL.cs
+++ ControleContasAPI/Util/DAL.cs
@@ -12,10 +12,10 @@ namespace ControleContasAPI.Util
         private static string Server = "servermysql55.mysql.database.azure.com";
         private static string Database = "controle_contas";
         private static string User = "cintiazago@servermysql55";
-        private static string Password = "Admin@123";
+        private static string Password = "";
         private MySqlConnection Connection;
 
-        private string ConnectionString = $"Server={Server};Database={Database};Uid={User};Pwd={Password};Sslmode=Required;charset=utf8";
+        private string ConnectionString = $"Server={Server};Database={Database};Uid={User};Pwd={Password};Sslmode=equired;charset=utf8";
 
         public DAL()
         {            
